feb26cad4217df8f2881be7a0def62853dc3c5b4
Bogere Siraje <bogeresiraje25@gmail.com>
2021-04-29 21:07:47 +0300

    Create finobacci and factorial

--- src/components/question_1/Factorial.js
+++ src/components/question_1/Factorial.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '../common/Container';
 
 
-export default function Factorial(){
+export default function Factorial() {
+    const [number, setNumber] = useState('');
+    const [factorial, setFactorial] = useState('');
+
+    const calculateFactorial = () => {
+        const fact = num => {
+            if(num < 0){
+                return 'Invalid number';
+            }
+
+            if (num === 0) {
+                return 1;
+
+            } else {
+                return num * fact(num - 1);
+            }
+        }
+
+        setFactorial(fact(number));
+    }
+
     return (
         <Container>
             <div className="br-mainpanel">
@@ -14,15 +34,29 @@ export default function Factorial(){
                         <div className="col-12">
                             <div className="card pd-0 bd-0 shadow-base">
                                 <div className="pd-x-30 pd-t-30 pd-b-15">
-                                    <div className="d-flex align-items-center justify-content-between">
-                                    </div>{/* d-flex */}
+                                    <div className="align-items-center justify-content-between">
+                                        <div className="form-group row">
+                                            <label htmlFor="num">Enter Number</label>
+                                            <input
+                                                type="number"
+                                                className="form-control"
+                                                value={number}
+                                                onChange={(e) => setNumber(e.target.value)}
+                                            />
+                                        </div>
+
+                                        <div className="form-group row">
+                                            <button className="btn btn-success" onClick={calculateFactorial}>Print Factorial</button>
+                                        </div>
+                                        <h3>{factorial}</h3>
+                                    </div>
                                 </div>
                                 <div className="pd-x-15 pd-b-15">
                                 </div>
-                            </div>{/* card */}
-                        </div>{/* col-9 */}
-                    </div>{/* row */}
-                </div>{/* br-pagebody */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </Container>
     );
--- src/components/question_1/Finobacci.js
+++ src/components/question_1/Finobacci.js
@@ -1,9 +1,26 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '../common/Container';
 
 
 export default function Finobacci() {
+    const [number, setNumber] = useState('');
+    const [fino, setFino] = useState('');
+
+    const getFinobacci = () => {
+        let n1 = 0, n2 = 1, nextTerm;
+        let seq = "0, 1";
+
+        for (let i = 1; i <= number; i++) {
+            nextTerm = n1 + n2;
+            n1 = n2;
+            n2 = nextTerm;
+            seq = `${seq}, ${nextTerm}`;
+        }
+
+        setFino(seq);
+    }
+
     return (
         <Container>
             <div className="br-mainpanel">
@@ -15,7 +32,21 @@ export default function Finobacci() {
                         <div className="col-12">
                             <div className="card pd-0 bd-0 shadow-base">
                                 <div className="pd-x-30 pd-t-30 pd-b-15">
-                                    <div className="d-flex align-items-center justify-content-between">
+                                    <div className="align-items-center justify-content-between">
+                                        <div className="form-group row">
+                                            <label htmlFor="num">Enter Number</label>
+                                            <input
+                                                type="number"
+                                                className="form-control"
+                                                value={number}
+                                                onChange={(e) => setNumber(e.target.value)}
+                                            />
+                                        </div>
+
+                                        <div className="form-group row">
+                                            <button className="btn btn-success" onClick={getFinobacci}>Print Finobacci Sequence</button>
+                                        </div>
+                                        <h3>{fino}</h3>
                                     </div>{/* d-flex */}
                                 </div>
                                 <div className="pd-x-15 pd-b-15">
